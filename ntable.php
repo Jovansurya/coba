09bdf35934404a20c2b0b53f6f238a7abc2fe6af
jovansurya <jovansurya97@gmail.com>
2021-11-09 13:55:26 +0700

    add db

---
+++ ntable.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect("localhost", "root", "", "pweb");
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
